f846f7b264e608545a5d9c0b285385cbf9d369b1
asif03 <babu033045@gmail.com>
2025-04-28 16:42:08 +0600

    Init code

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,15 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  layout,
+  route,
+  index,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  layout("routes/admin/admin-layout.tsx", [
+    route("dashboard", "routes/admin/dashboard.tsx"),
+    route("all-users", "routes/admin/all-users.tsx"),
+  ]),
+
+  layout("routes/root/layout.tsx", [index("routes/root/home.tsx")]),
+] satisfies RouteConfig;
