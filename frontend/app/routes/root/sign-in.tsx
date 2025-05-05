1dbf5a91783b92d9236df33cd5af4c713abd725b
Asif <babu033045@gmail.com>
2025-05-05 22:51:31 +0600

    Adding appwrite

---
+++ frontend/app/routes/root/sign-in.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <main className="auth">
+      <section className="size-full glassmorphism flex justify-center items-center"></section>
+    </main>
+  );
+};
+
+export default SignIn;
